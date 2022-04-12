a755b678d11ff9284c5e1b004222fad17b880b1a
Eder Pagliotto <ederpagliotto@gmail.com>
2022-04-12 19:33:12 +0100

    board size, buttons clear and set configured

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+// Determines board size
+function populateBoard(size){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "green";
-    board.insertAdjacentElement("beforeend", square);
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        } )
+        square.style.backgroundColor = "green";
+        board.insertAdjacentElement("beforeend", square);
+    }
 }
+populateBoard(16);
+
+document.querySelector(".set").addEventListener("click", () => {
+    let boardSize = document.querySelector(".show-size").innerHTML;
+    populateBoard(boardSize);
+});
+
+function setSize(value) {
+    document.querySelector(".show-size").innerHTML = value;
+}
+
+
+
+   document.querySelector(".clear").addEventListener("click", () => {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    let boardSize = document.querySelector(".show-size").innerHTML;
+    populateBoard(boardSize);
+   });
